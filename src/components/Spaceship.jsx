1a0d6b94e38da1c68445472265cd11804f807f6a
Rajdeep <rajdeep3004@gmail.com>
2022-11-08 16:28:02 +0530

    3rd commit

--- src/components/Spaceship.jsx
+++ src/components/Spaceship.jsx
@@ -10,7 +10,7 @@ const Spaceship = () => {
     <Fragment>
       <div className="spaceship-div">
         <Zoom>
-          <h1 className="h1-spaceship">Best rides in business.</h1>
+          <h1 className="h1-spaceship">Best rides in business</h1>
           <p className="p">
             Our ships are equipped with all necessary features for space travels
             such as systems to live and breathe, proper propulsion, the ability
@@ -22,7 +22,7 @@ const Spaceship = () => {
 
       <div className="child-div-spaceship">
         <Zoom cascade>
-          <h1 className="child-div-spaceship-h1">go with a team....</h1>
+          <h1 className="child-div-spaceship-h1">go with a team..</h1>
         </Zoom>
         <Fade right>
           <div>
